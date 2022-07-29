d36348ccd07e127b0b2c9c12ad7b0c101c1f5420
sonisaransh02 <60021305+sonisaransh02@users.noreply.github.com>
2022-07-30 01:53:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=abda8592";
+const API_URL = {API_URL};
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
